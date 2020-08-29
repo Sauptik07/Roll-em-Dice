8a462523a8ee944569bc5af4dd096b5a9b38d533
Sauptik07 <47649258+Sauptik07@users.noreply.github.com>
2020-08-29 22:54:49 +0530

    Adding files 1.0

---
+++ index.js
@@ -0,0 +1,29 @@
+function startRoll(){
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //Generating 1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //Getting a random dice img
+
+var randomImageSource1 = randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins! 🔥";
+
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins! 🔥";
+
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw 🚩";
+
+}}
